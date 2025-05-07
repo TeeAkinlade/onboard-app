cc0a915ada6346de31b31c9191ec3b52aa1c4395
TeeAkinlade <taiye.akinlade1@gmail.com>
2025-05-07 14:29:52 +0100

    Changes

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,7 +7,7 @@ import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div className="w-full relative">
+    <div className="w-full relative pb-4">
       <div className="flex flex-col items-center pt-20 bg-[#50C2C9] w-full mb-4">
           <Image
               src={profile}
@@ -22,7 +22,7 @@ export default function Home() {
           <Navbar />
       </div>
       <AnalogClock />
-      <h2 className="text-sm font-semibold mb-4 px-6">Task List</h2>
+      <h2 className="text-sm font-semibold mb-8 px-6">Task List</h2>
       <div className="bg-white pt-10 pb-4 mx-6 px-6 shadow-lg shadow-[#00000040] rounded-lg">
           <div className="flex justify-between items-center mb-7">
               <h2 className="text-xs font-semibold text-center">Daily Task</h2>
